18f1a2622d467e5b424690b92b9139fe5cdf2966
kinghoo <jinhu.huangjh@01bees.com>
2019-09-29 16:17:19 +0800

    auto commit with shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,73 @@
+/* eslint-disable import/extensions */
 import * as React from 'react';
 import * as _ from 'lodash';
-import { Row, Col } from 'antd';
+import { Row, Col, Icon, Tag } from 'antd';
+import * as hljs from 'highlight.js';
 import { HashRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
 import { urlParser } from './utils';
 import './App.scss';
 
-const context = (require as any).context('./Components', true, /doc\.tsx/);
+const context = (require as any).context('./Components', true, /doc\.tsx$/);
 const componentDocs: string[] = context.keys();
 
-class Wrapper extends React.Component<any, any> {
+const totalContext = (require as any).context(
+  './Components',
+  true,
+  /[^(doc)]\.((tsx)|(md)|(scss)|(ts)|(js))$/,
+);
+const totalContextKey = totalContext.keys();
+
+class WrapProps {
+  public path: string;
+}
+class WrapState {
+  public files: string[] = [];
+  public doc: string = '';
+  public isDocShow: boolean = false;
+  public sourceCode: def.ICommonObj = {};
+  public selectedSource: string = '';
+}
+class Wrapper extends React.Component<WrapProps, WrapState> {
+  public static defaultProps = new WrapProps();
+
+  public state = new WrapState();
+
+  public componentDidMount() {
+    this.handleSelectKeys(this.props);
+  }
+
+  public componentWillReceiveProps(nextProps: WrapProps) {
+    this.handleSelectKeys(nextProps);
+  }
+
+  public handleSelectKeys = (props: WrapProps) => {
+    const keys: string[] = [];
+    totalContextKey.forEach((key: string) => {
+      if (this.props.path === key.split('/')[1]) {
+        keys.push(key);
+      }
+    });
+    this.setState({ files: keys }, () => this.handleData(props));
+  };
+
+  public handleData = (props: WrapProps) => {
+    fetch(`/db-comp/src/Components/${props.path}/doc.tsx`)
+      .then(res => res.text())
+      .then(doc => this.setState({ doc }));
+    this.state.files.forEach(file => {
+      fetch(`/db-comp/src/Components/${file.replace(/^\.\//, '')}`)
+        .then(res => res.text())
+        .then(text => {
+          this.setState({
+            sourceCode: {
+              ...this.state.sourceCode,
+              [file]: text,
+            },
+          });
+        });
+    });
+  };
+
   public render() {
     const urlObj = urlParser();
     return (
@@ -29,7 +88,81 @@ class Wrapper extends React.Component<any, any> {
           })}
         </Col>
         <Col span={18} className="right-panel">
-          {this.props.children}
+          <div className="code-box">
+            <div className="code-box-demo">{this.props.children}</div>
+            <div className="code-box-actions">
+              <Tag
+                color={this.state.isDocShow ? '#2db7f5' : ''}
+                onClick={() => {
+                  this.setState({
+                    isDocShow: !this.state.isDocShow,
+                  });
+                }}
+                style={{ cursor: 'pointer' }}
+              >
+                查看demo代码
+                <Icon type="code" title="展示demo源代码" />
+              </Tag>
+            </div>
+            <div
+              className={`highlight-wrapper ${
+                this.state.isDocShow ? 'active' : ''
+              }`}
+            >
+              <div className="highlight">
+                <pre>
+                  <code
+                    className="hljs"
+                    dangerouslySetInnerHTML={{
+                      __html: hljs.highlightAuto(this.state.doc).value,
+                    }}
+                  />
+                </pre>
+              </div>
+            </div>
+            <div className="code-box-actions">
+              {this.state.files.map(item => {
+                return (
+                  <Tag
+                    key={item}
+                    style={{ cursor: 'pointer' }}
+                    color={this.state.selectedSource === item ? '#2db7f5' : ''}
+                    onClick={() => {
+                      this.setState({
+                        selectedSource:
+                          this.state.selectedSource === item ? '' : item,
+                      });
+                    }}
+                  >
+                    查看{item}
+                    <Icon type="code" title={`展示${item}源代码`} />
+                  </Tag>
+                );
+              })}
+            </div>
+            <div
+              className={`highlight-wrapper ${
+                this.state.selectedSource ? 'active' : ''
+              }`}
+            >
+              <div className="highlight">
+                <pre>
+                  <code
+                    className="hljs"
+                    dangerouslySetInnerHTML={{
+                      __html: hljs.highlightAuto(
+                        _.get(
+                          this.state.sourceCode,
+                          this.state.selectedSource,
+                          '',
+                        ),
+                      ).value,
+                    }}
+                  />
+                </pre>
+              </div>
+            </div>
+          </div>
         </Col>
       </Row>
     );
@@ -58,7 +191,7 @@ export default class App extends React.Component<any, any> {
             const path = item.replace(/^\./, '').replace(/\.tsx$/, '');
             const Component = context(item).default;
             const Comp = () => (
-              <Wrapper>
+              <Wrapper path={item.split('/')[1]}>
                 <Component />
               </Wrapper>
             );
--- src/Components/Db-Table/doc.tsx
+++ src/Components/Db-Table/doc.tsx
@@ -1,20 +1,7 @@
 import * as React from 'react';
-import { Divider } from 'antd';
 import Component from './index';
 
 export default class TestDoc extends React.Component<any, any> {
-  public state = {
-    code: '',
-    doc: '',
-  };
-  public componentDidMount() {
-    fetch('/db-comp/src/Components/Db-Table/index.tsx')
-      .then(res => res.text())
-      .then(text => this.setState({ code: text }));
-    fetch('/db-comp/src/Components/Db-Table/doc.tsx')
-      .then(res => res.text())
-      .then(text => this.setState({ doc: text }));
-  }
   public render() {
     return (
       <div>
@@ -26,12 +13,6 @@ export default class TestDoc extends React.Component<any, any> {
             },
           ]}
         />
-        <Divider />
-        <h3>db-table使用方法</h3>
-        <pre>{this.state.doc}</pre>
-        <Divider />
-        <h3>db-table源代码</h3>
-        <pre>{this.state.code}</pre>
       </div>
     );
   }
