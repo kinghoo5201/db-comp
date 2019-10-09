0f0cc7b0e1dd3e8b9ce9c6cfe70a33d03cb76dd2
kinghoo <jinhu.huangjh@01bees.com>
2019-10-09 17:11:41 +0800

    auto commit with shell

---
+++ src/Components/Db-Util/doc.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { Divider } from 'antd';
+import util from './index';
+
+export default class Doc extends React.Component<any, any> {
+  public render() {
+    return (
+      <div>
+        <h3>util方法</h3>
+        <p>包含各种通用方法</p>
+        <Divider />
+        <ul>
+          <li>
+            urlParser:解析url（hash-router）
+            <p>
+              util.urlParser('/github.com/#/index?cc=dd') ::
+              {JSON.stringify(util.urlParser('/github.com/#/index?cc=dd'))}
+            </p>
+          </li>
+          <li>
+            getUID:获取唯一id
+            <p>util.getUID() :: {util.getUID()}</p>
+          </li>
+          <li>
+            getNextItem: 循环获取一个序列中的下一项
+            <p>
+              const arr=['#1','#2','#3']; <br />
+              const getNextItem=util.getNextItem()(arr); <br />
+              getNextItem();
+              <br /> getNextItem();
+              <br />
+              getNextItem();
+              <br /> getNextItem();
+            </p>
+            <p>
+              {(() => {
+                const arr = ['#1', '#2', '#3'];
+                const getNextItem = util.getNextItem()(arr);
+                return (
+                  <>
+                    {getNextItem()}
+                    <br />
+                    {getNextItem()}
+                    <br />
+                    {getNextItem()}
+                    <br />
+                    {getNextItem()}
+                  </>
+                );
+              })()}
+            </p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ src/Components/Db-Util/index.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import * as util from '../../utils';
+
+export default util;
