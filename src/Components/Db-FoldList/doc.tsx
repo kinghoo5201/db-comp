3a827f61a8eea44e9541694885c69446d13cb501
cxx1431 <xiaoxia.chenxx@01bees.com>
2019-10-12 11:03:23 +0800

    update

--- src/Components/Db-FoldList/doc.tsx
+++ src/Components/Db-FoldList/doc.tsx
@@ -8,9 +8,15 @@ export default class Doc extends React.Component<any, any> {
     const rate = _.get(item, '列2', '');
 
     return (
-      <div>
-        <span>{name}</span>
-        <span>{rate}</span>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: '1.5rem',
+        }}
+      >
+        <span style={{ position: 'absolute', left: '10px' }}>{name}</span>
+        <span style={{ position: 'absolute', left: '100px' }}>{rate}</span>
       </div>
     );
   };
@@ -19,7 +25,10 @@ export default class Doc extends React.Component<any, any> {
       <div>
         <div>
           <h3>列表折叠组件</h3>
-          <p>列表选择组件，支持多项数据选项，以列表样式展示</p>
+          <p>
+            列表选择组件，支持多项数据选项，以列表样式展示，
+            <b>其中，每一行的行内数据和样式的展现可由使用者自行定义</b>
+          </p>
           <Component
             title="我是标题"
             initDataLength={5}
@@ -58,6 +67,11 @@ export default class Doc extends React.Component<any, any> {
               },
             ]}
             itemRender={this.renderGuantongItem}
+            customExpandText={{
+              expand: { text: 'expand', icon: 'left' },
+              fold: { text: 'fold', icon: 'right' },
+            }}
+            justifyContentType="center"
           />
           <h3>列表折叠组件</h3>
           <p>列表选择组件，直接渲染组件内部子元素，这个不进行折叠</p>
--- src/Components/Db-FoldList/index.tsx
+++ src/Components/Db-FoldList/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable no-nested-ternary */
 import * as React from 'react';
 import * as _ from 'lodash';
 import { Icon } from 'antd-mobile';
@@ -9,19 +10,41 @@ interface ListItem {
   [props: string]: string | number;
 }
 
+interface TextAndIcon {
+  text?: string;
+  icon?: string;
+}
+
+interface ExpandText {
+  expand: TextAndIcon;
+  fold: TextAndIcon;
+}
+
 export class FoldListProps {
   /** 初始显示的项数 */
   public initDataLength?: number = 5;
   /** 折叠组件的标题名称 */
   public title?: string = '';
+  /** 标题的位置 */
+  public justifyContentType?:
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around' = 'center';
   /** 折叠组件的数据 */
   public dataSource?: ListItem[] = [];
-  /** 每列要显示的键名 */
-  // public colInfo?: string[] = [];
   /** 每列要渲染成的样式的处理方法 */
   public itemRender?: (val: ListItem) => {};
   /** 折叠面板的子元素 */
   public children?: React.ReactNode;
+  /** 组件自定义添加的类名 */
+  public className?: string;
+  /** 要显示的 展示/折叠 的字体，默认 展开/折叠 */
+  public customExpandText?: ExpandText = {
+    expand: { text: '展开', icon: 'up' },
+    fold: { text: '折叠', icon: 'down' },
+  };
 }
 
 class FoldListState {
@@ -50,6 +73,9 @@ export default class List extends React.Component<
       dataSource,
       children,
       itemRender,
+      className,
+      customExpandText,
+      justifyContentType,
     } = this.props;
     let data: ListItem[] = [];
     if (expand) {
@@ -59,29 +85,33 @@ export default class List extends React.Component<
     } else {
       data = dataSource;
     }
-    // const data = expand ? dataSource : initDataLength ? dataSource.slice(0, initDataLength) : dataSource;
+
     const canExpand = initDataLength
       ? initDataLength < dataSource.length
       : false;
-    window.console.log('cxxx', expand, canExpand);
     return (
-      <div className="bees-list">
-        <div className="bees-list-content">
+      <div className={`fold-list ${className || ''}`}>
+        <div className="fold-list-content">
           {title && (
-            <div className="bees-list-item-wrap bees-list-title">
-              <div className="bees-list-item">{title}</div>
+            <div className="fold-list-item-wrap fold-list-title">
+              <div
+                className="fold-list-item"
+                style={{ justifyContent: justifyContentType || 'center' }}
+              >
+                <span>{title}</span>
+              </div>
             </div>
           )}
 
           {children ||
             data.map((item, index) => {
               return itemRender ? (
-                <div className="bees-list-item-wrap" key={index}>
-                  <div className="bees-list-item">{itemRender(item)}</div>
+                <div className="fold-list-item-wrap" key={index}>
+                  <div className="fold-list-item">{itemRender(item)}</div>
                 </div>
               ) : (
-                <div className="bees-list-item-wrap" key={index}>
-                  <div className="bees-list-item">
+                <div className="fold-list-item-wrap" key={index}>
+                  <div className="fold-list-item">
                     {_.get(item, `${title}`, '')}
                   </div>
                 </div>
@@ -90,11 +120,31 @@ export default class List extends React.Component<
         </div>
 
         {canExpand && (
-          <div className="expand" onClick={this.switch}>
-            {expand ? '折叠' : '展开'}
+          <div className="fold-list-expand" onClick={this.switch}>
+            <span
+              style={{
+                verticalAlign: 'text-bottom',
+              }}
+            >
+              {expand
+                ? customExpandText && !_.isEmpty(customExpandText)
+                  ? _.get(customExpandText, 'expand.text', '')
+                  : '折叠'
+                : customExpandText && !_.isEmpty(customExpandText)
+                ? _.get(customExpandText, 'fold.text', '')
+                : '展开'}
+            </span>
             <span style={{ position: 'relative' }}>
               <Icon
-                type={expand ? 'up' : 'down'}
+                type={
+                  expand
+                    ? customExpandText && !_.isEmpty(customExpandText)
+                      ? _.get(customExpandText, 'expand.icon', '')
+                      : 'up'
+                    : customExpandText && !_.isEmpty(customExpandText)
+                    ? _.get(customExpandText, 'fold.icon', '')
+                    : 'down'
+                }
                 size="xs"
                 style={{ position: 'absolute', top: '.08rem', left: 0 }}
               />
